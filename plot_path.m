ae8fb00d0eef0eb64ee5dfeb8e39437cd0843979
K G <krashagig.@gmail.com>
2021-04-12 11:11:41 -0400

    Initial commit

---
+++ plot_path.m
@@ -0,0 +1,76 @@
+%this contains a non-viable path.  FIX ME!!
+
+function  plot_path (path_start,path_end,a_vec)
+
+via_1=[-pi;0];
+
+wrap_12=[pi;0];
+ figure(2)
+ hold on
+ plot(path_start(1),path_start(2),'g.',"markersize",50)
+ plot(path_end(1),path_end(2),'g.',"markersize",50)
+
+dq_size=0.1
+dist1 = norm(path_start-via_1)%% WHAT IS THIS WEIRD METHOD
+dist2 = norm(wrap_12-path_end)
+
+  
+  
+npts = round(dist1/dq_size)
+fignum=3
+figure(fignum)
+clf
+[obstacle_ctr_pts,obstacle_radii] = get_environment_model;
+plot_circles(obstacle_ctr_pts,obstacle_radii,fignum)
+fignum=3;
+figure(fignum)
+for i=0:npts
+  figure(3)
+clf
+plot_circles(obstacle_ctr_pts,obstacle_radii,fignum)
+hold on
+axis([-3,3,-3,3])
+grid on
+axis('square')
+  q_vec = path_start+(via_1-path_start)*i/npts;
+  [arm_ctr_pts,arm_model_radii] = get_arm_collision_model(q_vec,a_vec);
+[payload_ctr_pts,payload_radii] = get_payload_collision_model(q_vec,a_vec);
+
+arm_ctr_pts=[arm_ctr_pts,payload_ctr_pts];
+arm_model_radii=[arm_model_radii,payload_radii];
+plot_circles(arm_ctr_pts,arm_model_radii,fignum)
+axis([-3,3,-3,3])
+ figure(2)
+ hold on
+ plot(q_vec(1),q_vec(2),'k.',"markersize",30)
+pause
+end
+  
+
+  
+npts = round(dist2/dq_size);
+
+for i=0:npts
+  figure(3)
+clf
+plot_circles(obstacle_ctr_pts,obstacle_radii,fignum)
+hold on
+axis([-3,3,-3,3])
+grid on
+axis('square')  
+  q_vec = wrap_12+(path_end-wrap_12)*i/npts;
+  [arm_ctr_pts,arm_model_radii] = get_arm_collision_model(q_vec,a_vec);
+[payload_ctr_pts,payload_radii] = get_payload_collision_model(q_vec,a_vec);
+
+arm_ctr_pts=[arm_ctr_pts,payload_ctr_pts];
+arm_model_radii=[arm_model_radii,payload_radii];
+plot_circles(arm_ctr_pts,arm_model_radii,fignum)
+ figure(2)
+  hold on
+
+ plot(q_vec(1),q_vec(2),'k.',"markersize",30)
+pause
+end
+
+
+end
